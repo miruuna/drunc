1524a874854313001bd9283fd0e6826ac0a9642a
Miruna Serian <mirunaserian@gmail.com>
2025-11-27 12:22:49 +0000

    create __init__ if it doesn't exist

--- docs/utils/generate_ghpages_docstrings.py
+++ docs/utils/generate_ghpages_docstrings.py
@@ -21,7 +21,8 @@ for path in root_folder.rglob("*.py"):
     if not init_file.exists():
         init_file.parent.mkdir(parents=True, exist_ok=True)
         init_file.write_text("# Package initializer\n")
-
+    print("path", path)
+    print("path parent ", path.parent)
     print("init exists", init_file.exists())
     # Module and documentation paths relative to root
     module_path = path.relative_to(root_folder).with_suffix("")
